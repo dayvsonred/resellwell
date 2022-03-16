d4de60540461fb5e736e9ddc7dbb70a396ae876b
dayvson_menezes_local <dayvison.assis@gmail.com>
2022-03-15 23:28:01 -0300

    login and dash ok

--- app/src/main/java/com/example/resellwell/MainActivity.java
+++ app/src/main/java/com/example/resellwell/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.resellwell;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.resellwell.activity.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        callLogin();
+    }
+
+
+    private void callLogin(){
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
---
+++ app/src/main/java/com/example/resellwell/activity/login/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.example.resellwell.activity.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.resellwell.R;
+import com.example.resellwell.activity.dashboard.DashboardActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btEntrar = findViewById(R.id.bt_login_entra);
+
+
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callDashboard();
+            }
+        });
+    }
+
+
+
+    private void callDashboard(){
+        startActivity(new Intent(this, DashboardActivity.class ));
+    }
+
+}
